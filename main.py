c4d06ed5ee1048366f5592eaea8469340959c61f
tommyjanna <25873341+tommyjanna@users.noreply.github.com>
2021-10-08 17:49:43 -0400

    updated from depracated selenium parameter

--- main.py
+++ main.py
@@ -10,74 +10,80 @@ import sys
 import time
 import random
 
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from selenium import webdriver
 
-waitPageLoad = 10
-waitBetweenRafflesMin = 2.5
-waitBetweenRafflesMax = 7.5
+# Time parameters
+wait_page_load = 10
+wait_between_raffles_min = 2.5
+wait_between_raffles_max = 7.5
 
-def rSleep(minTime: float, maxTime: float):
+def random_sleep(time_min: float, time_max: float):
     '''
     Sleep for a random amount of time within an interval
 
     Parameters:
-        minTime (float): Lower time interval bound
-        maxTime (float): Upper time interval bound
+        time_min (float): Lower time interval bound
+        time_max (float): Upper time interval bound
     '''
 
-    timeToSleep = random.uniform(minTime, maxTime)
-    time.sleep(timeToSleep)
+    time_to_sleep = random.uniform(time_min, time_max)
+    time.sleep(time_to_sleep)
 
 
-if __name__ == '__main__':
-    # User defined path to web driver
-    driver_path = './geckodriver'
-    if len(sys.argv) > 1:
-        driver_path = sys.argv[1]
+# User defined path to web driver
+driver_path = './geckodriver'
+if len(sys.argv) > 1:
+    driver_path = sys.argv[1]
 
-    driver = webdriver.Firefox(executable_path=driver_path)
-    driver.get('https://scrap.tf/raffles')
+serv = Service(driver_path)
+driver = webdriver.Firefox(service=serv)
+driver.get('https://scrap.tf/raffles')
 
-    print('Please sign in through Steam, then press enter to begin...')
-    input()
+print('Please sign in through Steam, then press enter to begin...')
+input()
 
-    # Make sure user is logged in
-    if not driver.find_elements_by_xpath('//li[@class="dropdown nav-userinfo"]'):
-        print('Steam login not detected!', file=sys.stderr)
-        exit(1)
+# Make sure user is logged in
+if not driver.find_elements_by_xpath('//li[@class="dropdown nav-userinfo"]'):
+    print('Steam login not detected!', file=sys.stderr)
+    sys.exit(1)
 
-    # Scroll to the bottom of page until all raffles have loaded
-    while not driver.find_elements_by_xpath('//*[contains(text(), "That\'s all, no more!")]')[0].is_displayed():
-        print('Loading more raffles...')
-        driver.execute_script('window.scrollTo(0, document.body.scrollHeight - 200)')
-        rSleep(0.5, 1.5)
+# Scroll to the bottom of page until all raffles have loaded
+while not driver.find_elements_by_xpath('//*[contains(text(), "That\'s all, no more!")]')[0].is_displayed():
+    print('Loading more raffles...')
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight - 200)')
+    random_sleep(0.5, 1.5)
 
-    # Get the non-entered raffles
-    # Entered raffle:      <... class="panel-raffle raffle-entered" ...>
-    # Non-entered raffle:  <... class="panel-raffle " ...>
-    anchorList = driver.find_elements_by_xpath('//div[@class="panel-raffle "]/div/div/a')
+# Get the non-entered raffles
+# Entered raffle:      <... class="panel-raffle raffle-entered" ...>
+# Non-entered raffle:  <... class="panel-raffle " ...>
+anchor_list = driver.find_elements_by_xpath('//div[@class="panel-raffle "]/div/div/a')
 
-    # Get URL for each raffle element
-    raffles = [anchor.get_attribute('href') for anchor in anchorList]
+# Get URL for each raffle element
+raffles = [anchor.get_attribute('href') for anchor in anchor_list]
 
-    print('Found', len(raffles), 'unentered raffles!') 
+print('\nFound', len(raffles), 'unentered raffles!')
 
-    # Loop through all raffle URLs
-    for raffle in raffles:
-        driver.get(raffle)
+# Loop through all raffle URLs
+for raffle in raffles:
+    driver.get(raffle)
 
-        # Attempt to find and click raffle entry button
-        try:
-            WebDriverWait(driver, waitPageLoad).until(EC.element_to_be_clickable((By.XPATH, '//div[contains(@class, "enter-raffle-btns")][1]'))).click()
-        except TimeoutException as exception:
-            print('Raffle entry button could not be found...' + str(exception), file=sys.stderr)
-        except Exception:
-            print('Unexpected exception...')
+    # Attempt to find and click raffle entry button
+    try:
+        WebDriverWait(driver, wait_page_load).until(EC.element_to_be_clickable((By.XPATH, '//div[contains(@class, "enter-raffle-btns")][1]'))).click()
+    except TimeoutException:
+        print('Raffle entry button could not be found...', file=sys.stderr)
+        # Skip sleep step because program has already waited 10 seconds.
+        continue
+    except Exception as exception:
+        print('Unexpected exception...', str(exception), file=sys.stderr)
+    else:
+        print('.', end='')
 
-        rSleep(waitBetweenRafflesMin, waitBetweenRafflesMax)
+    rSleep(wait_between_raffles_min, wait_between_raffles_max)
 
-    print('Execution completed')
+print('Execution completed')
